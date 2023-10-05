fc1a0dd02934cf2533521618b6786b3be2d95851
TgNestorDev <nestor.tigasi93@gmail.com>
2023-10-05 14:37:34 -0500

    validación de token JWT con función

--- src/main/java/com/colegioeducativo/colegio/controllers/AlumnoController.java
+++ src/main/java/com/colegioeducativo/colegio/controllers/AlumnoController.java
@@ -1,6 +1,7 @@
 package com.colegioeducativo.colegio.controllers;
 import com.colegioeducativo.colegio.dao.AlumnoDao;
 import com.colegioeducativo.colegio.models.Alumno;
+import com.colegioeducativo.colegio.security.JWTSecurity;
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -15,10 +17,27 @@ public class AlumnoController {
     @Autowired
     private AlumnoDao alumnoDao;
 
+    @Autowired
+    private JWTSecurity jwtSecurity;
+
+    @GetMapping(value = "api/alumnos")
+    public ResponseEntity<List<Alumno>> getAlumnos(@RequestHeader(value = "Authorization") String tokenR){
+
+        if (!validarTokenDeFront(tokenR)){
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+        return new ResponseEntity<>(alumnoDao.getAlumnos(), HttpStatus.OK);
+    }
+
+    private boolean validarTokenDeFront(String token){
+        String idUsuario=jwtSecurity.getKey(token); //con get key obtendo el id del usuario que contiene el token
+        return idUsuario!=null;
+    }
+    /*sin token
     @GetMapping(value = "api/alumnos")
     public ResponseEntity<List<Alumno>> getAlumnos(){
         return new ResponseEntity<>(alumnoDao.getAlumnos(), HttpStatus.OK);
-    }
+    }*/
 
     @PostMapping(value = "api/alumnos")
     public ResponseEntity postAlumno(@RequestBody Alumno alumno){
